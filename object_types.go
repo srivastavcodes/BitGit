f207352d6aed4d9f9d1c87056296c8b429754be3
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-07-20 23:59:09 +0530

    type skeleton written
    Took 5 minutes

--- object_types.go
+++ object_types.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type GitBlob struct {
 	BlobData []byte
 }
@@ -13,4 +15,40 @@ func (blob *GitBlob) Type() string { return "blob" }
 func (blob *GitBlob) Deserialize(data []byte) error {
 	blob.BlobData = data
 	return nil
+}
+
+type GitTree struct{}
+
+func (blob *GitTree) Serialize(_ *GitRepository) ([]byte, error) {
+	return nil, fmt.Errorf("unimplemented object")
+}
+
+func (blob *GitTree) Type() string { return "tree" }
+
+func (blob *GitTree) Deserialize(data []byte) error {
+	return fmt.Errorf("unimplemented object")
+}
+
+type GitCommit struct{}
+
+func (blob *GitCommit) Serialize(_ *GitRepository) ([]byte, error) {
+	return nil, fmt.Errorf("unimplemented object")
+}
+
+func (blob *GitCommit) Type() string { return "commit" }
+
+func (blob *GitCommit) Deserialize(data []byte) error {
+	return fmt.Errorf("unimplemented object")
+}
+
+type GitTag struct{}
+
+func (blob *GitTag) Serialize(_ *GitRepository) ([]byte, error) {
+	return nil, fmt.Errorf("unimplemented object")
+}
+
+func (blob *GitTag) Type() string { return "tag" }
+
+func (blob *GitTag) Deserialize(data []byte) error {
+	return fmt.Errorf("unimplemented object")
 }
